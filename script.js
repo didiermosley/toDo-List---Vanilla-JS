988dc5d826594d61dc97fa45e470df24fbfd17d5
Didier <blessingmosley@hotmail.com>
2022-03-07 23:25:04 -0500

    This version has some great updates

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const items = document.getElementById('items');
 const input = document.getElementById('input');
+const edit = document.getElementById('edit');
+const remove = document.getElementById('remove');
+
+let n = 0;
 
 const addTask = () =>{
     const tasks = document.createElement("DIV");
     tasks.classList.add('item');
 
     const taskName = document.createElement("SPAN");
+    taskName.classList.add('task-n');
     taskName.textContent = input.value;
     input.value='';
 
@@ -14,10 +19,12 @@ const addTask = () =>{
 
     const create = document.createElement("ion-icon");
     create.setAttribute("name", "create");
-    create.setAttribute("onClick", () => console.log('testing'));
+    create.setAttribute("id","edit");
+    create.setAttribute("id","edit");
 
     const trash = document.createElement("ion-icon");
     trash.setAttribute("name", "trash");
+    trash.setAttribute("id","remove");
 
     btnContainer.appendChild(create);
     btnContainer.appendChild(trash);
@@ -26,6 +33,12 @@ const addTask = () =>{
     items.appendChild(tasks);
 }
 
-const editTask = () =>{
-    
-}
+edit.addEventListener("click",e=>{
+    if (e.target.getAttribute("name") == "create") {
+        document.querySelector('.task-n').setAttribute("contenteditable", "true");
+        e.target.setAttribute("name", "repeat");
+    } else {
+        e.target.setAttribute("name", "create");
+        document.querySelector('.task-n').setAttribute("contenteditable", "false");
+    }
+})
